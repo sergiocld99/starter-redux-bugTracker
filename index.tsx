5f205ee5dd4abf3c1a02fa0896247cad90fb71e4
Sergio Calderón <bs.sergiocalderon@gmail.com>
2024-05-16 17:24:43 -0300

    Redux Practice to Root Index

--- index.tsx
+++ index.tsx
@@ -7,3 +7,23 @@ if (!container) throw new Error("Root Element not found")
 
 const root = ReactDOM.createRoot(container);
 root.render(<App/>);
+
+// npx webpack --watch
+// live-server
+
+import { addBug, removeBug, solveBug } from "./src/redux/actions"
+import { store } from "./src/redux/store"
+
+console.log("Hola Mundo!")
+
+const unsuscribe = store.subscribe(() => {
+    console.log("Change detected in Store", store.getState())
+})
+
+store.dispatch(addBug("Mi primer bug"))
+store.dispatch(addBug("Un segundo bug"))
+store.dispatch(removeBug(1))
+store.dispatch(addBug("Un tercer bug"))
+store.dispatch(solveBug(2))
+
+unsuscribe();
